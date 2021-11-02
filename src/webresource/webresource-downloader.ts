f8f6d0a17386b981a37da1ce0f9a6725094bc7c0
Manuel Steiner <manuels@bluewin.ch>
2021-11-02 10:47:49 +0100

    downloading a webresource is now possible

---
+++ src/webresource/webresource-downloader.ts
@@ -0,0 +1,92 @@
+import * as vscode from "vscode";
+import { DefaultAzureCredential } from "@azure/identity";
+import * as fs from 'fs';
+import { DynamicsRequests } from "../connection/dynamics-requests";
+import { Helpers } from "../helpers";
+import { title } from "process";
+
+export interface WebResourceRecord {
+    [key: string]: string;
+}
+
+export class WebResourceDownloader {
+
+    _prefix: string = "";
+    _webResourceFolder: string = "";
+    _data: any = null;
+    _selectedFile: string = "";
+    _rootPath: string;
+    _connection: DefaultAzureCredential;
+    _configFileLocation: string;
+    _fullPath : string = "";
+    _dynamicsRequest : DynamicsRequests;
+
+    constructor(workSpaceRootPath: string) {
+        this._rootPath = workSpaceRootPath;
+        this._connection = new DefaultAzureCredential();
+        this._configFileLocation = this._rootPath + '/dynamicsConfig.json';
+        this._dynamicsRequest = new DynamicsRequests(this._connection);
+    }
+
+    downloadWebResourceContext(folder: any) {
+        this._fullPath = folder.path;
+        this._webResourceFolder = folder.path;
+        this.getConfigData();
+        if(this._data === null) { return; }
+        Helpers.determinePrefix(this._data).then((prefix) => {
+            this._prefix = prefix;
+            let filter = `filter=startswith(name,'${this._prefix}')`;
+            this._dynamicsRequest.getWebResource(filter).then((webResources) => {
+                if(webResources.value.length === 0) {
+                    vscode.window.showErrorMessage("No webresources found.");
+                }
+                else {
+                    let options = webResources.value.map((w: any) => w.name);
+                    let records: WebResourceRecord = {};
+                    
+                    webResources.value.forEach((w: any) => {
+                        records[w.name] = w.content;
+                    });
+                    this.selectResource(options, records);
+                }
+            });
+        });
+    }
+
+    selectResource(options: any, records: any) {
+        vscode.window.showQuickPick(options, { canPickMany: false , title: "Please select the webresource to download"}).then(displayName => {
+            if(!displayName) { return; }
+            let webResourceContent = records[displayName];
+            this.createNewWebResourceFile(displayName, webResourceContent);
+        });
+    }
+
+    createNewWebResourceFile(displayname: string, content: string) {
+        const writeData = Buffer.from(content, 'base64');
+        let fileName = displayname.replace(/^.*[\\\/]/, '');
+        vscode.workspace.fs.writeFile(vscode.Uri.file(this._fullPath + '/' + fileName), writeData);
+        vscode.window.showInformationMessage('Created a new webResourceFile');
+    }
+
+    setUpRequiredVariables() {
+        return new Promise<void>((resolve, reject) => {
+            this.getConfigData();
+            if(this._data === null) { return; }
+            Helpers.determinePrefix(this._data).then((prefix) => {
+                this._dynamicsRequest._prefix = prefix;
+                this._prefix = prefix;
+                resolve();
+            });
+        });
+    }
+
+    getConfigData() {
+        if(fs.existsSync(this._configFileLocation)){
+            this._dynamicsRequest._data = JSON.parse(fs.readFileSync(this._configFileLocation, "utf-8"));
+            this._data = JSON.parse(fs.readFileSync(this._configFileLocation, "utf-8"));
+        }
+        else {
+            vscode.window.showErrorMessage(`No dynamicsConfig.json file was found. Please add one in ${this._rootPath}`);
+        }
+    }
+}
