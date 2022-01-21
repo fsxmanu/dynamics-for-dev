a1befc2e9d85ee22c1cf135024d84859f81729f2
Manuel Steiner <manuels@bluewin.ch>
2022-01-21 15:57:24 +0100

    removed check if empty was selected

--- src/helpers.ts
+++ src/helpers.ts
@@ -26,7 +26,7 @@ export class Helpers {
             }
             vscode.window.showQuickPick(options, { canPickMany: false, title: "Please select the Prefix which should be used" }).then(selectedPrefix =>
             {
-                if(!selectedPrefix){ resolve("new_"); }
+                //no check if selected because if selected empty it will be new_ which is wrong
                 resolve(selectedPrefix);
             });
         });
